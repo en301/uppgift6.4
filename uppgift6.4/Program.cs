5c0a282b402587cbcf33681a99fddd7d652e40b9
en301 <emilsjostrand1000@gmail.com>
2024-02-14 11:01:43 +0100

    Add project files.

---
+++ uppgift6.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace uppgift_6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal du vill höja upp, till exempel 5^2");
+            string texttal = Console.ReadLine();
+            string[] texttalarray = texttal.Split('^');
+            double bas = double.Parse(texttalarray[0]);
+            double potens = double.Parse(texttalarray[1]);
+            Console.WriteLine("Produkten blir "+ Potens(potens, bas));
+            Console.ReadKey();
+        }
+        static double Potens(double potens, double bas)
+        {
+            double sum = 1;
+            for (int i = 0; i < potens; i++)
+            {
+                sum *= bas;
+            }
+            return sum;
+        }
+    }
+}
